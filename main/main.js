73bd01e88668752d00a9750e92e5e862b6fd031b
AirClown <yu.shichao@foxmail.com>
2019-05-30 11:30:55 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,25 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(s) {
+	var a1 =["._.","...","._.","._.","...","._.","._.","._.","._.","._."];
+	var a2 =["|.|","..|","._|","._|","|_|","|_.","|_.","..|","|_|","|_|"];
+	var a3 =["|_|","..|","|_.","._|","..|","._|","|_|","..|","|_|","..|"];
+    var s1 = "";
+    var s2 = "";
+    var s3 = "";
+
+    for(var i = s.length - 1;i>=0;--i){
+    	var j = parseInt(s[i]);
+        if(i<s.length -1){
+            s1=" "+s1;
+            s2=" "+s2;
+            s3=" "+s3;
+        }
+    	s1 = a1[j]+s1;
+    	s2 = a2[j]+s2;
+    	s3 = a3[j]+s3;
+    }
+
+    s1+="\n";
+    s2+="\n";
+    s3+="\n";
+    return s1+s2+s3;
 };
